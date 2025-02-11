8c62b58cf30dea938e4749c68930985db44fdc16
Maria Romualdo <90730177+mromualdo77@users.noreply.github.com>
2025-02-11 12:41:59 -0300

    Add files via upload
    Construindo Pokédes com JS

---
+++ Pokedex-JS/README.md
@@ -0,0 +1,3 @@
+# Trilha JS Developer - Pokedex
+teste modificação
+# https://github.com/mromualdo77/js-developer-pokedex.git
---
+++ Pokedex-JS/assets/js/poke-api.js
@@ -0,0 +1,47 @@
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response => response.json()))
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = async (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    try {
+        const response = await fetch(url)
+        const jsonBody = await response.json()
+        const pokemons = jsonBody.results
+        const detailRequests = pokemons.map(pokeApi.getPokemonDetail)
+        const pokemonsDetails = await Promise.all(detailRequests)
+        return pokemonsDetails
+    } catch (error) {
+        return console.error(error)
+    }
+}
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4')
+]).then((results) => {
+    console.log(results)
+})
